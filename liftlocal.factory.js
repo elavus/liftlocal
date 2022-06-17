52d285ef02373d16faebde79ccc6269e227a3d17
elavus <56485581+elavus@users.noreply.github.com>
2022-06-17 18:05:20 -0400

    Update liftlocal.factory.js

--- liftlocal.factory.js
+++ liftlocal.factory.js
@@ -11,15 +11,26 @@
     };
 
     return service;
-
+    
     function getWODs(date, program) {
-      var promise = $http.get('https://spreadsheets.google.com/feeds/list/1d0XTG-gt72C76ns3o5gOP1bg1_27CUrPkX-kwRkjUN4/1/public/values?alt=json');
+      var promise = $http.get(getURL('WODs'));
       return promise;
     }
 
     function getSignups(date, program) {
-      var promise = $http.get('https://spreadsheets.google.com/feeds/list/1d0XTG-gt72C76ns3o5gOP1bg1_27CUrPkX-kwRkjUN4/2/public/values?alt=json');
+      var promise = $http.get(getURL('Form Responses'));
       return promise;
     }
+
+    function getURL(sheet) {
+      var api;
+
+      api = 'https://sheets.googleapis.com/v4/spreadsheets/1d0XTG-gt72C76ns3o5gOP1bg1_27CUrPkX-kwRkjUN4/values/' + sheet + '?key=AIzaSyBiLTaskmTWmp52vHzuXhp_k2CZ_XBnfSs';
+      return api;
+    }
+    
+    
+    
+    
   }
 })();
